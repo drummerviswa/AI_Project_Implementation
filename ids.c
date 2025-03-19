799f1cf76d13e92b0d92153bb15f40dfc5ec4ff7
Viswanathan P <drummerviswa@gmail.com>
2025-03-19 20:51:02 +0530

    Final Files ♾️😊

---
+++ ids.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include "graph.h"
+#include "stack.h"
+#define A 0
+#define B 1
+#define C 2
+#define D 3
+#define E 4
+#define F 5
+#define G 6
+#define H 7
+int init_graph(graph_t *g_p)
+{
+    addEdge(g_p, A, B, 1);
+    addEdge(g_p, A, C, 1);
+    addEdge(g_p, B, D, 1);
+    addEdge(g_p, C, E, 1);
+    addEdge(g_p, C, F, 1);
+    addEdge(g_p, D, G, 1);
+    addEdge(g_p, D, H, 1);
+    return 0;
+}
+int dls(graph_t *g_p, int root, int goal, int limit)
+{
+    int node, depth;
+    int to;
+    stack_t *s_p, *sd_p;
+    s_p = createStack(10);
+    sd_p = createStack(10);
+    pushStack(s_p, root);
+    pushStack(sd_p, 0);
+    while (!isEmptyStack(s_p))
+    {
+        printf("|");
+        node = popStack(s_p);
+        depth = popStack(sd_p);
+        int countervar = 0;
+        while (countervar < depth)
+        {
+            printf("-");
+            countervar++;
+        }
+        printf("%d - (depth % d)\n", node, depth);
+        if (node == goal)
+        {
+            printf("Goal node %d found at depth %d\n", goal, depth);
+            return 1;
+        }
+        if (depth < limit)
+        {
+            for (to = g_p->nodes - 1; to > 0; to--)
+            {
+                if (getEdge(g_p, node, to))
+                {
+                    pushStack(s_p, to);
+                    pushStack(sd_p, depth + 1);
+                }
+            }
+        }
+    }
+    printf("\n");
+    destroyStack(s_p);
+    destroyStack(sd_p);
+    return 0;
+}
+int main()
+{
+    graph_t *g_p;
+    int status, depth;
+    g_p = createGraph(8);
+    init_graph(g_p);
+    depth = 1;
+    displayGraph(g_p);
+    printf("Iterative Deepening Search - Depth Limited: %d\n", depth);
+    while (1)
+    {
+        printf("Depth: %d\n", depth);
+        status = dls(g_p, 0, 5, depth);
+        if (status == 1)
+            break;
+        else
+            depth++;
+    }
+    destroyGraph(g_p);
+    return 0;
+}
